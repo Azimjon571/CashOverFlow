676688243e3d921b5d442e93eedfb261206e77c3
Azimjon571 <azimjon.007@mail.ru>
2023-06-19 22:39:06 +0500

    INFRA: Initialize Unit Test Project

---
+++ CashOverFlow.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,14 @@
+﻿//=================================================
+// Copyrigh (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//=================================================
+
+using Xunit;
+
+namespace CashOverFlow.Tests.Unit
+{
+    internal class DeleteMe
+    {
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
