ac9c5880f495edee5eb914e0b33e291fb45a8d28
Azimjon571 <azimjon.007@mail.ru>
2023-06-19 15:34:47 +0500

    INFRA:Initialize API Project

---
+++ CashOverFlow/Startup.cs
@@ -0,0 +1,62 @@
+//=================================================
+// Copyrigh (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//=================================================
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace CashOverFlow
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)=>
+            Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var operApiInfo = new OpenApiInfo
+            {
+                Title = "CashOverFlow",
+                Version = "v1"
+            };
+
+            services.AddControllers();
+
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(
+                    name:"v1", 
+                    info:operApiInfo);
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment enviroment)
+        {
+            if (enviroment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+
+                app.UseSwaggerUI(option =>
+                    option.SwaggerEndpoint(
+                        url:"/swagger/v1/swagger.json",
+                        name:"CashOverFlow v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            
+            app.UseEndpoints(endpoints =>
+                endpoints.MapControllers());
+        }
+    }
+}
