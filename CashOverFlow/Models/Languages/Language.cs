09628c5f08bf28c7c067e059d0731f22fa645c67
Azimjon571 <azimjon.007@mail.ru>
2023-06-22 20:59:17 +0500

    DATA: Model & Migrate Language

---
+++ CashOverFlow/Models/Languages/Language.cs
@@ -0,0 +1,18 @@
+﻿//=================================================
+// Copyrigh (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//=================================================
+
+using System;
+
+namespace CashOverFlow.Models.Languages
+{
+    public class Language
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public LanguageType LanguageType  { get; set; }
+        public DateTimeOffset CreateDate { get; set; }
+        public DateTimeOffset UpdateDate { get; set; }
+    }
+}
---
+++ CashOverFlow/Models/Languages/LanguageType.cs
@@ -0,0 +1,15 @@
+﻿//=================================================
+// Copyrigh (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//=================================================
+
+namespace CashOverFlow.Models.Languages
+{
+    public enum LanguageType
+    {
+        Other,
+        Frontend,
+        Backend,
+        FullStack
+    }
+}
