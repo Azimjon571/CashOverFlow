fff10fc386c6b2c20bb1c48b18aa99f9e7b64cad
Azimjon571 <azimjon.007@mail.ru>
2023-06-27 14:16:19 +0500

    DATA: Model & Migrate Salary

---
+++ CashOverFlow/Migrations/20230623063044_CreateSalaryTables.cs
@@ -0,0 +1,40 @@
+﻿//=================================================
+// Copyrigh (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//=================================================
+
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CashOverFlow.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateSalaryTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Salaries",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Amount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    CreatedDate = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Salaries", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Salaries");
+        }
+    }
+}
---
+++ CashOverFlow/Models/Salaries/Salary.cs
@@ -0,0 +1,19 @@
+﻿//=================================================
+// Copyrigh (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//=================================================
+
+using System;
+
+namespace CashOverFlow.Models.Salary
+{
+    public class Salary
+    {
+        public Guid Id { get; set; }
+        public decimal Amount { get; set; }
+        public int Experience { get; set; } 
+        public DateTimeOffset CreatedDate { get; set; }
+        public Guid LocationId { get; set; }
+        public Guid JobId { get; set; }
+    }
+}
