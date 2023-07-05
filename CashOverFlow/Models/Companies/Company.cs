3ff52dee1e068349d33087ddd185cbffc32e3783
Azimjon571 <azimjon.007@mail.ru>
2023-07-05 13:43:24 +0500

    DATA: Model & Migrate Companies

---
+++ CashOverFlow/Models/Companies/Company.cs
@@ -0,0 +1,16 @@
+﻿//=================================================
+// Copyrigh (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//=================================================
+
+using System;
+
+namespace CashOverFlow.Models.Companies
+{
+    public class Company
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}
